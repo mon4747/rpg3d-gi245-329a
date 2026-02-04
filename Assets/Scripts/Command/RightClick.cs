d407c0c7584bce58efe23d6fb2ec8e77aa804c35
mon4747 <nattawut.mon2561@gmail.com>
2026-02-04 08:55:04 +0700

    Follow orders

--- Assets/Scripts/Command/RightClick.cs
+++ Assets/Scripts/Command/RightClick.cs
@@ -1,16 +1,64 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RightClick : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    public static RightClick instance;
+
+    
+    private Camera cam; 
+    public LayerMask layerMask; 
+    private LeftClick leftClick; 
+
+    
+    void Awake()
     {
         
+        leftClick = GetComponent<LeftClick>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        instance = this; 
+        cam = Camera.main; 
+        layerMask = LayerMask.GetMask("Ground", "Character", "Building");
+    }
+
+    
     void Update()
     {
         
+        if (Input.GetMouseButtonUp(1))
+        {
+            TryCommand(Input.mousePosition);
+        }
+    }
+
+    
+    private void TryCommand(Vector2 screenPos)
+    {
+        Ray ray = cam.ScreenPointToRay(screenPos);
+        RaycastHit hit;
+
+        
+        if (Physics.Raycast(ray, out hit, 1000, layerMask))
+        {
+            switch (hit.collider.tag)
+            {
+                case "Ground":
+                    
+                    CommandToWalk(hit, leftClick.CurChar);
+                    break;
+            }
+        }
+    }
+
+    // 14.4. สร้างเมธอดสำหรับสั่งตัวละครเดินขึ้นมา
+    private void CommandToWalk(RaycastHit hit, Character c)
+    {
+        if (c != null)
+        {
+            c.WalkToPosition(hit.point);
+        }
     }
 }
